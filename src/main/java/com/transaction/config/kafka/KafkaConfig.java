a29076a140e7aff4e6ffe8cef3f04006b5bc8258
krittamet <krittamateza@gmail.com>
2026-03-23 02:49:38 +0700

    Refactor KafkaConfig to include bootstrap server configuration and mark KafkaTemplate as primary

--- src/main/java/com/transaction/config/kafka/KafkaConfig.java
+++ src/main/java/com/transaction/config/kafka/KafkaConfig.java
@@ -1,11 +1,30 @@
 package com.transaction.config.kafka;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.apache.kafka.clients.admin.NewTopic;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.context.annotation.Primary;
+import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.listener.ContainerProperties;
+
+import lombok.RequiredArgsConstructor;
 
 @Configuration
 public class KafkaConfig {
+
+    @Value("${spring.kafka.bootstrap-servers}")
+    private String bootstrapServers;
 
     @Bean
     public NewTopic transactionTopic() {
@@ -33,6 +52,7 @@ public class KafkaConfig {
     }
 
     @Bean
+    @Primary
     public KafkaTemplate<String, String> kafkaTemplate() {
         return new KafkaTemplate<>(producerFactory());
     }
